2a18d0452a57adc1dc0e1c50062a48066c4e2cdd
xavier <omondix214@gmail.com>
2023-05-26 13:01:19 +0300

    initial

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import * as Notifications from "expo-notifications";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export default function PushTokenGenerator() {
+  const [expoPushToken, setExpoPushToken] = useState(null);
+
+  useEffect(() => {
+    const registerForPushNotificationsAsync = async () => {
+      try {
+        const { status } = await Notifications.requestPermissionsAsync();
+
+        if (status !== "granted") {
+          alert("Failed to get push token for push notification!");
+          return;
+        }
+
+        const expoPushToken = (await Notifications.getDevicePushTokenAsync())
+          .data;
+        console.log("Expo Push Token:", expoPushToken);
+
+        setExpoPushToken(expoPushToken);
+      } catch (error) {
+        console.log("Error getting push token:", error);
+      }
+    };
+
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {expoPushToken ? (
+        <Text>Your Expo Push Token: {expoPushToken}</Text>
+      ) : (
+        <Text>Loading...</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +52,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
